55e34171a2671784f332fb8284cd84771dc8d2e0
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:26:41 -0500

    Update PetList.jsx

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,1 +1,16 @@
+import { Grid } from "@mui/material";
+import PetCard from "./PetCard";
 
+const PetList = ({ pets }) => {
+  return (
+    <Grid container spacing={2}>
+      {pets.map((pet) => (
+        <Grid item xs={12} md={4} key={pet.id}>
+          <PetCard pet={pet} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default PetList;
