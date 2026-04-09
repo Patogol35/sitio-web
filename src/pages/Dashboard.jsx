529ed6399f344053ffb9fb58cf1a92c856ea60cc
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:29:41 -0500

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,17 @@
+import { Container } from "@mui/material";
+import usePets from "../hooks/usePets";
+import PetList from "../components/PetList";
+import AddPetForm from "../components/AddPetForm";
 
+const Dashboard = () => {
+  const { pets, addPet } = usePets();
+
+  return (
+    <Container sx={{ my: 4 }}>
+      <AddPetForm addPet={addPet} />
+      <PetList pets={pets} />
+    </Container>
+  );
+};
+
+export default Dashboard;
