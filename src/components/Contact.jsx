193ee04f41de8413917c089cf9a044c53c2ffd33
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:30:34 -0500

    Create Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <Container sx={{ my: 4 }}>
+      <Typography variant="h4">Contacto</Typography>
+      <Typography>Email: vet@mail.com</Typography>
+    </Container>
+  );
+};
+
+export default Contact;
