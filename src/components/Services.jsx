3fe2f2116324f59c64d01de68b24bbe0bf6f81f3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:27:58 -0500

    Create Services.jsx

---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+
+const Services = () => {
+  return (
+    <Container sx={{ my: 4 }}>
+      <Typography variant="h4">Servicios</Typography>
+      <Typography>
+        Consultas, vacunas, cirugías y peluquería
+      </Typography>
+    </Container>
+  );
+};
+
+export default Services;
