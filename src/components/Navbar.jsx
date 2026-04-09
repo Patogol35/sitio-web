4e0ee926757eaaabddb477edc01fa9883dd0e1a0
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:40:14 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">Veterinaria 🐾</Typography>
+    <AppBar position="sticky" sx={{ background: "#2E7D32" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+          🐾 VetCare
+        </Typography>
+
+        <Box>
+          <Button color="inherit" component={Link} to="/">
+            Inicio
+          </Button>
+          <Button color="inherit" component={Link} to="/dashboard">
+            Mascotas
+          </Button>
+          <Button color="inherit" component={Link} to="/contacto">
+            Contacto
+          </Button>
+        </Box>
+
       </Toolbar>
     </AppBar>
   );
