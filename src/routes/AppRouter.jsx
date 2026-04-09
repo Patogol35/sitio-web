55c7157eda15c99649e29923cf80ecda7c4bc594
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:48:46 -0500

    Update AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,23 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Dashboard from "../pages/Dashboard";
-
-// ✅ Opción 1 (si NO moviste el archivo)
 import Contact from "../components/Contact";
-
-// ✅ Opción 2 (si lo moviste a pages, usa esta en lugar de la de arriba)
-// import Contact from "../pages/Contact";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/contacto" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/contacto" element={<Contact />} />
+    </Routes>
   );
 };
 
