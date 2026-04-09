dea540366b15d759b1cdf12ac868e3b10b94b9f4
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-09 09:26:27 -0500

    Update PetCard.jsx

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,1 +1,14 @@
+import { Card, CardContent, Typography } from "@mui/material";
 
+const PetCard = ({ pet }) => {
+  return (
+    <Card sx={{ borderRadius: 3 }}>
+      <CardContent>
+        <Typography variant="h6">{pet.name}</Typography>
+        <Typography>{pet.type}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PetCard;
