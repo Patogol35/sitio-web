be4940ca2641f389156058b9c4e529035c25b359
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-11 19:17:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import CartDrawer from "./components/CartDrawer";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  return (
+    <>
+      {/* Navbar */}
+      <Navbar openCart={() => setIsCartOpen(true)} />
+
+      {/* Página principal */}
+      <Home />
+
+      {/* Carrito lateral */}
+      <CartDrawer
+        isOpen={isCartOpen}
+        closeCart={() => setIsCartOpen(false)}
+      />
+    </>
+  );
 }
 
 export default App;
