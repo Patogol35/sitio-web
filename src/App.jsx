7befe78cc8ef0aeb788b3599353b8b51a8fcfc75
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-11 19:05:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import AppRouter from "./routes/AppRouter";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-      <Footer />
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
