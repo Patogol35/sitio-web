f98adce533af162f447b6f801bd12f26238f96e3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-11 19:26:22 -0500

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -4,6 +4,7 @@ const products = [
     name: "Laptop Gamer",
     price: 1200,
     category: "Laptops",
+    stock: 5,
     image: "https://via.placeholder.com/200"
   },
   {
@@ -11,6 +12,7 @@ const products = [
     name: "Mouse RGB",
     price: 25,
     category: "Accesorios",
+    stock: 10,
     image: "https://via.placeholder.com/200"
   },
   {
@@ -18,6 +20,7 @@ const products = [
     name: "Teclado Mecánico",
     price: 80,
     category: "Accesorios",
+    stock: 7,
     image: "https://via.placeholder.com/200"
   }
 ];
