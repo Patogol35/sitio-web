c8a3514ca38092325f4f99e637cfaa7fb546515d
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-11 19:17:17 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// IMPORTANTE
+import { CartProvider } from "./context/CartContext";
+
+// MUI (opcional pero recomendado)
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "light", // luego puedes poner dark 🔥
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#00c853",
+    },
+  },
+  shape: {
+    borderRadius: 12,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <CartProvider>
+        <App />
+      </CartProvider>
+
+    </ThemeProvider>
   </React.StrictMode>
 );
