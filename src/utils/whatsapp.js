49a5567bab86a60a40c4bcbe41803701d11448aa
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-11 19:03:48 -0500

    Update whatsapp.js

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,1 +1,11 @@
+export const sendToWhatsApp = (product) => {
+  const phone = "593997979099"; 
 
+  const message = `Hola, quiero comprar:
+Producto: ${product.name}
+Precio: $${product.price}`;
+
+  const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+  window.open(url, "_blank");
+};
